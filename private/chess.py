5bfc900d3a7dceeff9c4c0b4c3dfd6baaf4110b6
sw245 <qtd2012@gmail.com>
2025-05-14 18:29:16 +0900

    asd

--- private/chess.py
+++ private/chess.py
@@ -6,14 +6,22 @@ print("chessboard")
 # idea
 # 1. 행렬?
 # 2. 이중 리스트 출력 >> 이게 간단할 듯?
+# 3. blessed
 
+import blessed
+term = blessed.Terminal()
+
+# print(term.height)
+# print(term.width)
+
+# print(term.clear)
 
 row = ['1','2','3','4','5','6','7','8',]
 column = ['a','','b','','c','','d','','e','','f','','g','','h']
 
 for c in column:
-    print(c)
-print('\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(*row))
+    print(term.white(c))
+print(term.gray('\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(*row)))
 
 
 
