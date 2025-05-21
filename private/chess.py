a43e456e97fbeebf97dc86ca31b6fff8a7c6d852
sw245 <qtd2012@gmail.com>
2025-05-21 18:31:42 +0900

    chesssss

--- private/chess.py
+++ private/chess.py
@@ -18,97 +18,100 @@ term = blessed.Terminal()
 # print(term.clear)
 
 
+
 ### 객체 지정 ###
 
-number = ['8',' ','7',' ','6',' ','5',' ','4',' ','3',' ','2',' ','1']
+board_number = [' ','8',' ','7',' ','6',' ','5',' ','4',' ','3',' ','2',' ','1']
 alphabet = ['a','b','c','d','e','f','g','h']
 
 # rows = [
 #     [' ',' ',' ',' ',' ',' ',' ',' '] for _ in range(8)
 # ]
 
-rows = [
-    [' ']*8 for _ in range(8)
-]
+# rows = [
+#     [] for _ in range(8)
+# ]
+
+rows = {}
+
+for i in range(8):
+    for x in alphabet:
+        rows[f'{x}{i+1}'] = ' '
+        
+        # if i == 1-1: rows[1-1].append({f'{x}{i+1}':' '})
+        # elif i == 2-1: rows[2-1].append({f'{x}{i+1}':' '})
+        # elif i == 3-1: rows[3-1].append({f'{x}{i+1}':' '})
+        # elif i == 4-1: rows[4-1].append({f'{x}{i+1}':' '})
+        # elif i == 5-1: rows[5-1].append({f'{x}{i+1}':' '})
+        # elif i == 6-1: rows[6-1].append({f'{x}{i+1}':' '})
+        # elif i == 7-1: rows[7-1].append({f'{x}{i+1}':' '})
+        # elif i == 8-1: rows[8-1].append({f'{x}{i+1}':' '})
+
+
+
+
 
 pawns_w = ['p','p','p','p','p','p','p','p']
 pieces_w = ['R','N','B','Q','K','B','N','R']
 
-pawns_r = ['p','p','p','p','p','p','p','p']
-pieces_r = ['R','N','B','K','Q','B','N','R']
+pawns_r = [term.red('p'),term.red('p'),term.red('p'),term.red('p'),term.red('p'),term.red('p'),term.red('p'),term.red('p')]
+pieces_r = [term.red('R'),term.red('N'),term.red('B'),term.red('K'),term.red('Q'),term.red('B'),term.red('N'),term.red('R')]
+
+moves = []
+
+
+
+
+
+
+# ### 칸에 기물 할당 ###
+
+
+## 초기 세팅 
+# print(rows[8-1])
+
+for i in range(8):
+    for n,x in enumerate(alphabet):
+        if i+1 == 1:
+            rows[f'{x}{i+1}'] = pieces_w[n]
+        elif i+1 == 2:
+            rows[f'{x}{i+1}'] = pawns_w[n]
+        elif i+1 == 7:
+            rows[f'{x}{i+1}'] = pawns_r[n]
+        elif i+1 == 8:
+            rows[f'{x}{i+1}'] = pieces_r[n]
 
 # print(rows)
-
-# ￣|￣
-# ￣￣
 
 
 ### 터미널 출력 부분 (그래픽) ###
 
-print(' '*4,end='')
-print('|',end='')
-print('￣'*31,end='')
-print('|')
-
-for r in number:
-    if r == '8':
-        print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[8-1]),end='')
-        print('  |')
-        
-    elif r == '7':
-        print(r,'  |',end='')
-        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[7-1]),end='')
-        print('|')
-        
-    elif r == '6':
-        print(r,'  |',end='')
-        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[6-1]),end='')
-        print('|')
-        
-    elif r == '5':
-        print(r,'  |',end='')
-        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[5-1]),end='')
-        print('|')
-        
-    elif r == '4':
-        print(r,'  |',end='')
-        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[4-1]),end='')
-        print('|')
-        
-    elif r == '3':
-        print(r,'  |',end='')
-        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[3-1]),end='')
-        print('|')
-        
-    elif r == '2':
-        print(r,'  |',end='')
-        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[2-1]),end='')
-        print('|')
-        
-    elif r == '1':
-        print(r,'  |',end='')
-        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[1-1]),end='')
-        print('|')
-        
-    elif r == ' ':
+for r in board_number:
+    if r == ' ':
         print(r,'  |',end='')
         print('￣￣￣|'*8)
-        # print('|')
+    else:
+        print(r,'  |',end='')
+        print(f'\t{rows[f'a{r}']}  |   {rows[f'b{r}']}  |   {rows[f'c{r}']}  |   {rows[f'd{r}']}  |\
+   {rows[f'e{r}']}  |   {rows[f'f{r}']}  |   {rows[f'g{r}']}  |   {rows[f'h{r}']}  ',end='')
+        print('|')
         
+    
+
 print(' '*4,'￣'*28)
-    
+
 print()
-print('\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(*alphabet))
+print('\t{}      {}      {}      {}      {}      {}      {}      {}  '.format(*alphabet))
+# print('\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(*alphabet))
 
-
+print()
+turn = 1
+# move = input("백 차례>>")
+        
+# if turn % 2 == 1:   # 백 턴
+#     moves.append(move)
+#     rows[move[-2:]] = move[0]
     
-# ### 칸에 기물 할당 ###
-
-# for row in rows:
-#     pass
 
 
-
-
-
+# print(rows)
