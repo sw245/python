c2fbca920beadc399ecabc1bfe0e6576fd15cc36
sw245 <qtd2012@gmail.com>
2025-05-21 09:32:43 +0900

    fun

--- private/chess.py
+++ private/chess.py
@@ -23,8 +23,12 @@ term = blessed.Terminal()
 number = ['8',' ','7',' ','6',' ','5',' ','4',' ','3',' ','2',' ','1']
 alphabet = ['a','b','c','d','e','f','g','h']
 
+# rows = [
+#     [' ',' ',' ',' ',' ',' ',' ',' '] for _ in range(8)
+# ]
+
 rows = [
-    [' ',' ',' ',' ',' ',' ',' ',' '] for _ in range(8)
+    [' ']*8 for _ in range(8)
 ]
 
 pawns_w = ['p','p','p','p','p','p','p','p']
@@ -35,6 +39,8 @@ pieces_r = ['R','N','B','K','Q','B','N','R']
 
 # print(rows)
 
+# ￣|￣
+# ￣￣
 
 
 ### 터미널 출력 부분 (그래픽) ###
@@ -52,58 +58,55 @@ for r in number:
         
     elif r == '7':
         print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[7-1]),end='')
-        print('  |')
+        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[7-1]),end='')
+        print('|')
         
     elif r == '6':
         print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[6-1]),end='')
-        print('  |')
+        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[6-1]),end='')
+        print('|')
         
     elif r == '5':
         print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[5-1]),end='')
-        print('  |')
+        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[5-1]),end='')
+        print('|')
         
     elif r == '4':
         print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[4-1]),end='')
-        print('  |')
+        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[4-1]),end='')
+        print('|')
         
     elif r == '3':
         print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[3-1]),end='')
-        print('  |')
+        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[3-1]),end='')
+        print('|')
         
     elif r == '2':
         print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[2-1]),end='')
-        print('  |')
+        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[2-1]),end='')
+        print('|')
         
     elif r == '1':
         print(r,'  |',end='')
-        print('\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}   |\t{}'.format(*rows[1-1]),end='')
-        print('  |')
+        print('\t{}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  |   {}  '.format(*rows[1-1]),end='')
+        print('|')
         
     elif r == ' ':
         print(r,'  |',end='')
-        print('￣'*31,end='')
-        print('|')
+        print('￣￣￣|'*8)
+        # print('|')
         
-    else:
-        print(r,'  |\t\t\t\t\t\t\t\t   |')
-        
-print(' '*4,'￣'*31)
+print(' '*4,'￣'*28)
     
 print()
 print('\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(*alphabet))
 
 
     
-### 칸에 기물 할당 ###
+# ### 칸에 기물 할당 ###
 
-for row in rows:
-    pass
+# for row in rows:
+#     pass
 
 
 
